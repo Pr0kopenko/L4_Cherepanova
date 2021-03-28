084065d3e380f6a5fe89dc832d0f0c6a08d8028e
Vika Cherepanova <cherepanova_v.s@yandex.ru>
2021-03-28 10:18:21 +0300

    Улучшена функция печати  по сравнению с первым ДЗ

--- L4_Cherepanova/main.swift
+++ L4_Cherepanova/main.swift
@@ -18,6 +18,7 @@ import Foundation
 enum Transmission {
     case manual, auto
 }
+
 enum WindowsState: String {
     case open = "Открыты"
     case closed = "Закрыты"
@@ -50,7 +51,6 @@ class Car {
             }
         }
     }
-    
     static var carCount = 0
 
     init (brand: String, releaseYear: Int, transmission: Transmission, windowsState: WindowsState, mileage: Int) {
@@ -60,14 +60,13 @@ class Car {
         self.windowsState = windowsState
         self.mileage = mileage
         Car.carCount += 1
-
     }
-    
+
     deinit {
             Car.carCount -= 1
         }
 
-    
+
     static func countInfo(){
             print("Выпущено \(self.carCount) автомобилей")
         }
@@ -81,7 +80,7 @@ class Car {
                 windowsState = .open
                 print("Окна открыты")
             }
-            
+
         case .closeWindows:
             if windowsState == .closed {
                 print("Окна и так закрыты")
@@ -90,25 +89,35 @@ class Car {
                 print("Окна закрыты")
             }
         default:
-            print("Ничего не изменилось")
+            print("Невозможно совершить запрошенное действие для этого автомобиля")
         }
     }
     
+    func printCarInfo () {
+        print("____________________")
+        print(self.brand)
+        print("Год выпуска: \(self.releaseYear)")
+        print("Трансмиссия: \(self.transmission)")
+        print("Окна \(self.windowsState.rawValue)")
+        print("Пробег: \(self.mileage) км")
+        
+    }
+
 }
 
 class TrunkCar: Car {
-    
+
     var trunkState: Bool
     let trunkVolume: Double
     var trunkVolumeFilled: Double
-    
+
     init (brand: String, releaseYear: Int, transmission: Transmission, windowsState: WindowsState, mileage: Int, trunkState: Bool, trunkVolume: Double, trunkVolumeFilled: Double) {
         self.trunkState = trunkState
         self.trunkVolume = trunkVolume
         self.trunkVolumeFilled = trunkVolumeFilled
         super.init(brand: brand, releaseYear: releaseYear, transmission: transmission, windowsState: windowsState, mileage: mileage)
     }
-    
+
     override func carChange (action: CarAction) {
         switch action {
         case .openWindows:
@@ -158,19 +167,24 @@ class TrunkCar: Car {
                 print("Невозможно выгрузить из автомобиля \(cargo) кг. В автомобиле всего \(trunkVolumeFilled) кг  груза")
             }
         default:
-            print("Ничего не изменилось")
+            print("Невозможно совершить запрошенное действие для этого автомобиля")
         }
     }
-    
-    
+    override func printCarInfo () {
+        super.printCarInfo()
+        print("Объём багажника: \(self.trunkVolume) кг")
+        print("Заполненность багажника: \(self.trunkVolumeFilled) кг")
+        print("Багажник \(self.trunkState == true ? "прицеплен" : "отцеплен")")
+    }
+
 }
-    
+
 
 class SportCar: Car {
-    
+
     var roofState: RoofState
     let gasMileage: Double
-    
+
     init (brand: String, releaseYear: Int, transmission: Transmission, windowsState: WindowsState, mileage: Int, roofState: RoofState, gasMileage: Double) {
         self.roofState = roofState
         self.gasMileage = gasMileage
@@ -200,108 +214,83 @@ class SportCar: Car {
                 roofState = .raised
                 print("Крыша кабриолета поднята")
             }
-            
+
         case .roofLowered:
             roofState = .lowered
             print("Крыша кабриолета опущена")
         default:
-            print("Ничего не изменилось")
+            print("Невозможно совершить запрошенное действие для этого автомобиля")
         }
     }
 
+    override func printCarInfo () {
+        super.printCarInfo()
+        print("Расход бензина: \(self.gasMileage) л на 100 км")
+        print("Крыша автомобиля \(self.roofState == .raised ? "поднята" : "опущена")")
+        print("____________________")
+    }
+    
 }
 
-func printCarInfo (car: Car) {
-    print(car.brand)
-    print("Год выпуска: \(car.releaseYear)")
-    print("Трансмиссия: \(car.transmission)")
-    print("Окна \(car.windowsState == .closed ? "закрыты" : "открыты")")
-    print("Пробег: \(car.mileage) км")
-    print("____________________")
-}
-
-
-func printTrunkCarInfo (car: TrunkCar) {
-    print(car.brand)
-    print("Год выпуска: \(car.releaseYear)")
-    print("Трансмиссия:\(car.transmission)")
-    print("Объём багажника: \(car.trunkVolume) кг")
-    print("Заполненность багажника: \(car.trunkVolumeFilled) кг")
-    print("Багажник \(car.trunkState == true ? "прицеплен" : "отцеплен")")
-    print("Окна \(car.windowsState == .closed ? "закрыты" : "открыты")")
-    print("Пробег: \(car.mileage) км")
-    print("____________________")
-}
-
-func printSportCarInfo (car: SportCar) {
-    print(car.brand)
-    print("Год выпуска: \(car.releaseYear)")
-    print("\(car.transmission)")
-    print("Окна \(car.windowsState == .closed ? "закрыты" : "открыты")")
-    print("Пробег: \(car.mileage) км")
-    print("Расход бензина: \(car.gasMileage) л на 100 км")
-    print("Крыша автомобиля \(car.roofState == .raised ? "поднята" : "опущена")")
-    print("____________________")
-}
 
 var car1 = Car (brand: "Polo", releaseYear: 2010, transmission: .auto, windowsState: .closed, mileage: 200)
 var car2 = Car (brand: "Golf", releaseYear: 2001, transmission: .manual, windowsState: .closed, mileage: 0)
 
 
-var trunkcar1 = TrunkCar (brand: "Jeep", releaseYear: 2010, transmission: .auto, windowsState: .open, mileage: 100, trunkState: true, trunkVolume: 2000.0, trunkVolumeFilled: 0.0)
-var trunkcar2 = TrunkCar (brand: "Toyota", releaseYear: 2000, transmission: .auto, windowsState: .open, mileage: 0, trunkState: false, trunkVolume: 2100.0, trunkVolumeFilled: 100.0)
+var trunkСar1 = TrunkCar (brand: "Jeep", releaseYear: 2010, transmission: .auto, windowsState: .open, mileage: 100, trunkState: true, trunkVolume: 2000.0, trunkVolumeFilled: 0.0)
+var trunkCar2 = TrunkCar (brand: "Toyota", releaseYear: 2000, transmission: .auto, windowsState: .open, mileage: 0, trunkState: false, trunkVolume: 2100.0, trunkVolumeFilled: 100.0)
 
-var sportcar1 = SportCar (brand: "Subaru", releaseYear: 2005, transmission: .auto, windowsState: .closed, mileage: 100, roofState: .lowered, gasMileage: 10.0)
-var sportcar2 = SportCar (brand: "Ferrary", releaseYear: 1990, transmission: .manual, windowsState: .closed, mileage: 1000, roofState: .raised, gasMileage: 12.0)
+var sportCar1 = SportCar (brand: "Subaru", releaseYear: 2005, transmission: .auto, windowsState: .closed, mileage: 100, roofState: .lowered, gasMileage: 10.0)
+var sportCar2 = SportCar (brand: "Ferrary", releaseYear: 1990, transmission: .manual, windowsState: .closed, mileage: 1000, roofState: .raised, gasMileage: 12.0)
 
 
-printCarInfo (car: car1)
-printCarInfo (car: car2)
+car1.printCarInfo()
+car2.printCarInfo()
 
 print("Попробуем изменить состояние окон в автомобилях основного класса")
 
 car1.carChange (action: .openWindows)
 car2.carChange(action: .closeWindows)
 print("Вот что у нас получилось")
-printCarInfo (car: car1)
-printCarInfo (car: car2)
+car1.printCarInfo()
+car2.printCarInfo()
 
 print("Проверим, как работает слушатель пробега, унаследованный спортивным автомобилем от родительского класса")
-printSportCarInfo(car: sportcar1)
+sportCar1.printCarInfo()
 print("Добавим автомобилю 2000 км пробега")
-sportcar1.mileage = 2000
+sportCar1.mileage = 2000
 print("_______________")
 print("Что изменилось?")
-printSportCarInfo(car: sportcar1)
+sportCar1.printCarInfo()
 
 print("Проверим, работает ли наша кабриолетная крыша")
-printSportCarInfo(car: sportcar1)
-printSportCarInfo(car: sportcar2)
+sportCar1.printCarInfo()
+sportCar2.printCarInfo()
 print("_______________")
-sportcar1.carChange(action: .roofRaised)
-sportcar1.carChange(action: .roofRaised)
+sportCar1.carChange(action: .roofRaised)
+sportCar1.carChange(action: .roofRaised)
 print("_______________")
-printSportCarInfo(car: sportcar1)
-printSportCarInfo(car: sportcar2)
+sportCar1.printCarInfo()
+sportCar2.printCarInfo()
 
 print("Поиграем с багажниками и их загрузкой у авто с прицепами")
-printTrunkCarInfo (car: trunkcar1)
-printTrunkCarInfo (car: trunkcar2)
+trunkСar1.printCarInfo()
+trunkCar2.printCarInfo()
 print("_______________")
-trunkcar1.carChange(action: .hookedTrunk)
-trunkcar1.carChange(action: .loadTrunk(cargo: 250))
-trunkcar2.carChange(action: .hookedTrunk)
-trunkcar2.carChange(action: .loadTrunk(cargo: 500))
+trunkСar1.carChange(action: .hookedTrunk)
+trunkСar1.carChange(action: .loadTrunk(cargo: 250))
+trunkCar2.carChange(action: .hookedTrunk)
+trunkCar2.carChange(action: .loadTrunk(cargo: 500))
 print("_______________")
 print("Что изменилось?")
-printTrunkCarInfo (car: trunkcar1)
-printTrunkCarInfo (car: trunkcar2)
+trunkСar1.printCarInfo()
+trunkCar2.printCarInfo()
 print("_______________")
 Car.countInfo()
 car2 = car1
 Car.countInfo()
 print("_______________")
 
-trunkcar2 = trunkcar1
+trunkCar2 = trunkСar1
 Car.countInfo()
-printTrunkCarInfo (car: trunkcar2)
+trunkCar2.printCarInfo()
